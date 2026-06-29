652895a270d9e4a5398faa141df83c75fce12343
krish <ky7366277@gmail.com>
2026-06-30 01:58:13 +0530

    Time: 27 ms (98.69%), Space: 20.6 MB (94.86%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def sortArray(self, nums):
+        nums.sort()
+        return nums
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
