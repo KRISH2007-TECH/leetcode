072ff05f555f46f8502109deda50b45d3bde34a3
krish <ky7366277@gmail.com>
2026-07-02 19:51:23 +0530

    Time: 0 ms (100%), Space: 12.5 MB (34.47%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -1,12 +1,7 @@
 class Solution(object):
     def runningSum(self, nums):
-        s=len(nums)
-        i=0
-        j=1
-        while(j<s):
-            nums[j]=nums[i]+nums[j]
-            i=i+1
-            j=j+1
+        for i in range (1,len(nums)):
+            nums[i]+=nums[i-1]
         return nums
 
 
