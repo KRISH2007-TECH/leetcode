ca73afd4caa6a2f1589a96a4f4d2fd95ed51c73a
krish <ky7366277@gmail.com>
2026-07-16 10:37:43 +0530

    Time: 0 ms (100%), Space: 12.4 MB (53.15%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,7 +1,9 @@
 class Solution(object):
     def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
-        
+        l = len(digits)
+        for i in range(l - 1, -1, -1):
+            digits[i] += 1
+            if digits[i] < 10:
+                return digits
+            digits[i] = 0
+        return [1] + digits
