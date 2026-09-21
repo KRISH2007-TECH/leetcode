76fb60e5ca053222d03c605162458127a1ba054f
krish <ky7366277@gmail.com>
2026-09-21 22:43:01 +0530

    Add solution for Reverse String

--- LeetCode/Reverse-String/Solution.py
+++ LeetCode/Reverse-String/Solution.py
@@ -1,9 +1,16 @@
 1class Solution(object):
 2    def reverseString(self, s):
-3        return s.reverse()
-4
-5        """
-6        :type s: List[str]
-7        :rtype: None Do not return anything, modify s in-place instead.
-8        """
-9        
+3        i=0
+4        n=(len(s)/2)-1
+5        j=len(s)-1
+6        while(i<j):
+7            temp=s[i]
+8            s[i]=s[j]
+9            s[j]=temp
+10            i+=1
+11            j-=1
+12        """
+13        :type s: List[str]
+14        :rtype: None Do not return anything, modify s in-place instead.
+15        """
+16        
